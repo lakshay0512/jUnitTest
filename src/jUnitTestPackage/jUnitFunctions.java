0816b50ca00af46c289c75f18d89ae28ef8f56ca
lakshay0512 <113931388+lakshay0512@users.noreply.github.com>
2022-11-21 18:44:59 +0530

    jUnitTesting functions

--- src/jUnitTestPackage/jUnitFunctions.java
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -1,21 +1,10 @@
 package jUnitTestPackage;
-import java.io.*;
-import java.util.*;
 public class jUnitFunctions {
-	public static void main(String[] args){
-        Scanner sn = new Scanner(System.in);
-        
-        System.out.println("Enter the first number:");
-        int x = sn.nextInt();
-        
-        System.out.println("Enter the second number:");
-        int y = sn.nextInt();
-        
-        int sum = sumFunction(x,y);
-        System.out.println("Sum is: " + sum);
+    public int addNumbers(int n1, int n2){
+        return n1 + n2;
     }
     
-    public static int sumFunction(int x, int y){
-        return x + y;
+    public String addStrings(String s1, String s2){
+        return s1 + s2;
     }
 }
